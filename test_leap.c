0c60d59e911adb2a090c455c9712b28549c99f6b
Texas-Alex <82863901+Texas-Alex@users.noreply.github.com>
2022-07-31 14:34:26 +0800

    Update test_leap.c

--- test_leap.c
+++ test_leap.c
@@ -8,13 +8,13 @@
  */
 int is_leap(long year) {
 
-  if (year % 4 != 0) {
+  if (year % 4 != 0  || year % 400 != 0) {
     return 0;
   }
 
-  if (year % 100 == 0) {
+  if (year % 100 != 0) {
     return 1;
-  }
+  
 
   return 0;
 }
